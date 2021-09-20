f812a8b238f39f2b943efe80cb2059c66466bd97
kuri0726 <82706716+kuri0726@users.noreply.github.com>
2021-09-20 16:01:32 +0900

    demo

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get new" do
     get new_contact_url
     assert_response :success
   end
+
 end
