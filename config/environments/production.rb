7648a9d9cbf3282e2da34d784ec85fcebb0cd676
kuri0726 <82706716+kuri0726@users.noreply.github.com>
2021-09-21 16:03:08 +0900

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://quiet-spire-24968.herokuapp.com'
+  host = 'https://pure-earth-35392.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
